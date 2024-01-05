bf4161ad7a3e0d8dc481e5645c11b13553d83683
Shazin <shazin.bd@gmail.com>
2024-01-05 17:39:42 +0600

    Added argp for parsing arguments

--- src/main.c
+++ src/main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include "utils.h"
 #include "args_parser.h"
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    // printHelp();
-    init_arg_parser();
-    printf("Total args: %i; first arg at pos 0: %s\n\n", argc, argv[0]);
-    // printf("Hello world \n");
+
     parse_args(argc, argv);
     return 0;
 }
